b0a932e2102d571613f44fc47a7beb9229140ca4
inspektoratmahulu29-beep <inspektoratmahulu29@gmail.com>
2026-09-12 22:25:12 +0800

    Delete api directory

--- api/public/revision.js
+++
@@ -1,5 +0,0 @@
-export async function onRequestGet() {
-  return new Response(JSON.stringify({revision: Math.floor(Date.now()/3000)*3000}), {
-    headers:{'content-type':'application/json','cache-control':'no-store'}
-  });
-}
